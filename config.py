08a3cca596e6f574c9ae0b464dcef8a7096aaf62
ziyosen <ziyosenstore@gmail.com>
2026-05-15 22:30:57 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # config.example.py
-API_ID = 14261613  # Ganti dengan API ID lo
-API_HASH = "88a95a8d947a92b9bacd4f7215e8ca36"  # Ganti dengan API HASH lo
-OWNER_ID = [7687084316]
+import os
 
+API_ID = int(os.environ.get("API_ID", 0))  
+API_HASH = os.environ.get("API_HASH", "")  
+OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "0").split()]
