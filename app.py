b78274a34193558c84e8e8b426e81eb51840f83f
ziyosen <ziyosenstore@gmail.com>
2026-05-16 00:16:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import pyromod # 
+import pyromod 
 from pyrogram import Client
-from config import API_ID, API_HASH, SESSION_STRING 
+from config import API_ID, API_HASH, SESSION_STRING
 
 app = Client(
-    "myuserbot",
+    "benxx_userbot",
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=SESSION_STRING,
-    workers=20,
-    plugins=dict(root="modules")
+    workers=20
 )
