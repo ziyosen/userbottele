27314db1a09f998b894709bbd2f25b83ac52264e
ziyosen <ziyosenstore@gmail.com>
2026-05-15 22:36:24 +0700

    Add SESSION_STRING to configuration

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# config.example.py
 import os
 
 API_ID = int(os.environ.get("API_ID", 0))  
 API_HASH = os.environ.get("API_HASH", "")  
 OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "0").split()]
+SESSION_STRING = os.environ.get("SESSION_STRING", "")
